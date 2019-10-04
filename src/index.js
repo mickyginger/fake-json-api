4d4f790ea3081714a715082f5110a4950a234b5f
mickyginger <mickyginger@gmail.com>
2019-10-04 14:24:17 +0100

    Something to do with the way that webpack is exporting the module means that the fake AJAX request is not behaving as expected

--- src/index.js
+++ src/index.js
@@ -1,32 +1,120 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { fakeXhr } from 'nise'
+import Dexie from 'dexie'
+import relationships from 'dexie-relationships'
 
-import schema from './schema.json'
-import initialData from './data.json'
+const db = new Dexie('local-json-api', { addons: [relationships] })
 
-import api from './fakeAPI'
+let _schema
 
-api.configure(schema)
-api.loaddata(initialData)
-api.listen()
+function configure(schema) {
 
-import axios from 'axios'
+  _schema = schema
 
-class App extends React.Component {
-
-  componentDidMount() {
-    axios.get('/users/1')
-      .then(res => console.log(res.data))
+  const stores = {}
+  for(const table in schema) {
+    stores[table] = schema[table].schema
   }
 
-  render() {
-    return (
-      <h1>Hello World!</h1>
-    )
+  db.version(1)
+    .stores(stores)
+}
+
+function loaddata(data) {
+  for(const store in data) {
+    db[store].count()
+      .then(count => {
+        if(!count) db[store].bulkPut(data[store])
+      })
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+function getRecords(table) {
+  return db[table].with(_schema[table].populate || {})
+}
+
+function getRecord(table, id) {
+  return db[table].where({ id: +id }).with(_schema[table].populate || {}).then(records => records[0])
+}
+
+function createRecord(table, data) {
+  return db[table].add(data)
+    .then(id => getRecord(table, id))
+}
+
+function updateRecord(table, id, data) {
+  return db[table].update(+id, data)
+    .then(() => getRecord(table, id))
+}
+
+function deleteRecord(table, id) {
+  return db[table].delete(+id)
+}
+
+function getPathInfo(path) {
+  return path.split('/').slice(1)
+}
+
+function jsonify(data) {
+  const jsonified = {}
+
+  Object.getOwnPropertyNames(data).forEach(key => {
+    jsonified[key] = typeof(key) === 'object' ? JSON.stringify(data[key]) : data[key]
+  })
+
+  return jsonified
+}
+
+function toJSON(data) {
+  return JSON.stringify(data instanceof Array ? data.map(jsonify) : jsonify(data))
+}
+
+var xhr = fakeXhr.useFakeXMLHttpRequest()
+
+function listen() {
+
+  xhr.onCreate = (xhr) => {
+
+    setTimeout(async () => {
+      let data = null
+      let status = 200
+
+      const [ table, id ] = getPathInfo(xhr.url)
+
+      if(xhr.method === 'GET') {
+        data = id ? await getRecord(table, id) : await getRecords(table)
+        if(!data) return xhr.respond(404)
+      }
+
+      if(xhr.method === 'POST') {
+        data = await createRecord(table, JSON.parse(xhr.requestBody))
+        status = 201
+      }
+
+      if(xhr.method === 'PUT') {
+        data = await updateRecord(table, id, JSON.parse(xhr.requestBody))
+        if(!data) return xhr.respond(404)
+      }
+
+      if(xhr.method === 'DELETE') {
+        data = await getRecord(table, id)
+        if(data) {
+          await deleteRecord(table, id)
+          return xhr.respond(204)
+        }
+
+        return xhr.respond(404)
+      }
+
+      xhr.respond(status, {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+      }, toJSON(data))
+    }, 500)
+  }
+}
+
+export default {
+  configure,
+  loaddata,
+  listen
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    globalObject: 'this'
   },
-  devtool: 'source-maps',
   module: {
     rules: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
   devServer: {
@@ -23,10 +23,6 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
+    new webpack.optimize.ModuleConcatenationPlugin()
   ]
 }
